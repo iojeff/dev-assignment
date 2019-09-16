96852b504e41da339ce43c6c5339150bf7c981ce
Jeffrey Wolff <jeff.wolff@spellingcity.com>
2019-09-15 20:15:32 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
+import '@fortawesome/fontawesome-free/css/all.css'
+import '../js/vendors/lazy-loader.js';
+import '../scss/app.scss';
