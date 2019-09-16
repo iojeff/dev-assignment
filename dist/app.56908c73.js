308c0b778dce2db385c5be413a3c3573d9a4e5bb
Jeffrey Wolff <jeff.wolff@spellingcity.com>
2019-09-16 00:36:45 -0400

    responsive style adjustments

--- dist/app.56908c73.js
+++ dist/app.56908c73.js
@@ -18393,7 +18393,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54592" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50158" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
